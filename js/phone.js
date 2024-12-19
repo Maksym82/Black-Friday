46d1c89b56d189947c7e1fc429bff41d97a057a9
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-12-19 19:12:03 +0200

    Update style consultation

--- js/phone.js
+++ js/phone.js
@@ -7,3 +7,12 @@ window.intlTelInput(input, {
       "https://cdn.jsdelivr.net/npm/intl-tel-input@25.2.0/build/js/utils.js"
     ),
 });
+
+intlTelInput(input, {
+  customPlaceholder: (selectedCountryPlaceholder, selectedCountryData) => "+380" + selectedCountryPlaceholder,
+});
+
+window.intlTelInput(input, {
+  initialCountry: "ua",
+  loadUtils: () => import("/intl-tel-input/js/utils.js?1733756310855") // for formatting/placeholders etc
+});
