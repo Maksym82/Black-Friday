b067a86a80e871fbc3144e8af4f0198e0935b965
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-12-30 18:41:46 +0200

    Update phone.js

--- js/phone.js
+++ js/phone.js
@@ -9,10 +9,11 @@ window.intlTelInput(input, {
 });
 
 intlTelInput(input, {
-  customPlaceholder: (selectedCountryPlaceholder, selectedCountryData) => "+380" + selectedCountryPlaceholder,
+  customPlaceholder: (selectedCountryPlaceholder, selectedCountryData) => "" + selectedCountryPlaceholder,
 });
 
 window.intlTelInput(input, {
   initialCountry: "ua",
+  separateDialCode: true,
   loadUtils: () => import("/intl-tel-input/js/utils.js?1733756310855") // for formatting/placeholders etc
 });
